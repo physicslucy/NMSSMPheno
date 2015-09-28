ef2567efc29dba2ad44af660648fa831d8c102b5
Robin Aggleton <robin.aggleton@cern.ch>
2015-09-28 22:24:44 +0100

    Small comments and printout edits

--- MG5_aMC/submitMG5_dag.py
+++ MG5_aMC/submitMG5_dag.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 """
-Submit lots of MG5 samples to condor as a DAG to allow easy monitoring and retry.
+Submit lots of MG5_aMC jobs to condor. All jobs run using the same input card.
+
+The user should specify an input card with MG5_aMC instructions, an output
+directory on /hdfs, the number of events to generate per job, and the
+total number of job to submit. There is also the option for a 'dry run' where
+all the files & directories are set up, but the job is not submitted.
+
+Note that this submits the jobs not one-by-one but as a DAG, to allow easier
+monitoring of job status.
 """
 
 
@@ -34,9 +42,9 @@ def submit(in_args=sys.argv[1:]):
                         action='store_true')
     args = parser.parse_args(in_args)
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Do some checks, make directories
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     if not os.path.isfile(args.card):
         raise RuntimeError("Cannot find specified card %s", args.card)
 
@@ -58,9 +66,9 @@ def submit(in_args=sys.argv[1:]):
     # File stem common for all condor, dag, status files
     file_stem = 'mg5_%s_%s_%s' % (channel, date, time)
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Make a condor job for this batch using the template
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     script_name = file_stem + '.condor'
     print 'Condor job file:', script_name
     with open('runMG5_aMC.condor', 'r') as template:
@@ -73,15 +81,16 @@ def submit(in_args=sys.argv[1:]):
     with open(script_name, 'w') as job_file:
         job_file.write(job_description)
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Make DAG file with the date and time
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     dag_name = file_stem + '.dag'
     status_name = file_stem + '.status'
     print "DAG file:", dag_name
     with open(dag_name, 'w') as dag_file:
         dag_file.write('# DAG for card %s\n' % args.card)
-        dag_file.write('# %d jobs each generating %d events\n' % (args.nJobs, args.nEvents))
+        dag_file.write('# Running %d jobs\n' % args.nJobs)
+        dag_file.write('# Each generating %d events\n' % args.nEvents)
         dag_file.write('\n')
         for job_ind in xrange(args.nJobs):
             job_name = '%s_%d' % (channel, job_ind)
@@ -92,13 +101,15 @@ def submit(in_args=sys.argv[1:]):
             dag_file.write('VARS %s opts="%s"\n' % (job_name, job_vars))
         dag_file.write('NODE_STATUS_FILE %s\n' % status_name)
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Submit it
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     if not args.dry:
         call(['condor_submit_dag', dag_name])
         print 'Check status with:'
-        print 'DAGstatus.py %s' % status_name
+        print 'DAGstatus.py', status_name
+        print ''
+        print 'Condor log files written to:', log_dir
 
 
 if __name__ == "__main__":
